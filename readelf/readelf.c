feb98984e819f435784be0969dd680f0bfa10a64
18373109 <refkxh@buaa.edu.cn>
2020-03-16 11:36:14 +0000

    Initial commit of lab1_extra

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -29,6 +29,35 @@ int is_elf_format(u_char *binary)
         return 0;
 }
 
+int is_big_endian(u_char *binary) {
+	Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
+	if (ehdr->e_ident[5] == 2) return 1;
+	return 0;
+}
+
+uint16_t get_16(uint16_t *ptr, int is_bigendian) {
+	u_char char1, char2;
+	if (is_bigendian) {
+		char1 = *(u_char *)ptr;
+		char2 = *((u_char *)ptr + 1);
+		return ((uint16_t)char1 << 8) + char2;
+	}
+	else return *ptr;
+}
+
+uint32_t get_32(uint32_t *ptr, int is_bigendian) {
+	u_char char1, char2, char3, char4;
+	if (is_bigendian) {
+		char1 = *(u_char *)ptr;
+		char2 = *((u_char *)ptr + 1);
+		char3 = *((u_char *)ptr + 2);
+		char4 = *((u_char *)ptr + 3);
+		return ((uint32_t)char1 << 24) + ((uint32_t)char2 << 16) +
+		   	((uint32_t)char3 << 8) + char4;
+	}
+	else return *ptr;
+}
+
 /* Overview:
  *   read an elf format binary file. get ELF's information
  *
@@ -47,13 +76,13 @@ int readelf(u_char *binary, int size)
 {
         Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
 
-        int Nr;
+        int Nr, endian;
 
-        Elf32_Shdr *shdr = NULL;
+        Elf32_Phdr *phdr = NULL;
 
-        u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count;
-        Elf32_Half sh_entry_size;
+        u_char *ptr_ph_table = NULL;
+        Elf32_Half ph_entry_count;
+        Elf32_Half ph_entry_size;
 
 
         // check whether `binary` is a ELF file.
@@ -62,18 +91,21 @@ int readelf(u_char *binary, int size)
                 return -1;
         }
 
-        // get section table addr, section header number and section header size.
-		shdr = binary + ehdr->e_shoff;
-		sh_entry_count = ehdr->e_shnum;
-		sh_entry_size = ehdr->e_shentsize;
+		endian = is_big_endian(binary);
 
-        // for each section header, output section number and section addr. 
+        // get program table addr, program header number and program header size.
+		phdr = binary + get_32(&ehdr->e_phoff, endian);
+		ph_entry_count = get_16(&ehdr->e_phnum, endian);
+		ph_entry_size = get_16(&ehdr->e_phentsize, endian);
+
+        // for each program header, output segment number, filesize and memsize. 
         // hint: section number starts at 0.
-		ptr_sh_table = shdr;
-		for (Nr = 0; Nr < sh_entry_count; Nr++) {
-			Elf32_Addr sh_addr = ((Elf32_Shdr *)ptr_sh_table)->sh_addr;
-			printf("%d:0x%x\n", Nr, sh_addr);
-			ptr_sh_table += sh_entry_size;
+		ptr_ph_table = phdr;
+		for (Nr = 0; Nr < ph_entry_count; Nr++) {
+			Elf32_Word filesize = get_32(&((Elf32_Phdr *)ptr_ph_table)->p_filesz, endian);
+			Elf32_Word memsize = get_32(&((Elf32_Phdr *)ptr_ph_table)->p_memsz, endian);
+			printf("%d:0x%x,0x%x\n", Nr, filesize, memsize);
+			ptr_ph_table += ph_entry_size;
 		}
 
         return 0;
