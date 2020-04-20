d4514f34a6629d21d86b9d62784a25fbac3bb608
18373109 <refkxh@buaa.edu.cn>
2020-04-20 14:42:55 +0000

    Modified set operations

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,55 +2,44 @@
 
 #include "pageReplace.h"
 
-#include <list>
 #include <map>
 #include <set>
+#include <stack>
 
 const unsigned int PAGE_SHIFT = 12;
 const unsigned int N_PHY_PAGE = 64;
 
 #define GET_PAGE(addr) (((unsigned long) (addr)) >> PAGE_SHIFT)
 
-std::list<int> freeList;  // physical index
+std::stack<int> freeStack;  // physical index
 std::map<long, std::pair<long, int>> frames;  // <page number, <last referenced time, physical index>>
 std::set<std::pair<long, long>> lastUsed;  // <last referenced time, page number>
 
 void pageReplace(long *physic_memory, long nwAdd) {
     static long clock = 0;
-    static bool isFirst = true;
     if (clock == 0) {
         for (int i = 0; i < N_PHY_PAGE; i++) {
-            freeList.push_back(i);
+            freeStack.push(i);
         }
     }
     long pageNum = GET_PAGE(nwAdd);
     if (frames.count(pageNum)) {
+        lastUsed.erase(std::pair<long, long>(frames[pageNum].first, pageNum));
         frames[pageNum].first = clock;
         lastUsed.insert(std::pair<long, long>(clock++, pageNum));
         return;
     }
-    if (freeList.empty()) {
-        while (true) {
-            auto victim = lastUsed.begin();
-            if (frames.count(victim->second) && victim->first == frames[victim->second].first) {
-                physic_memory[frames[victim->second].second] = 0;
-                freeList.push_back(frames[victim->second].second);
-                frames.erase(victim->second);
-                lastUsed.erase(victim);
-                break;
-            }
-            lastUsed.erase(victim);
-        }
+    if (freeStack.empty()) {
+        auto victim = lastUsed.begin();
+        physic_memory[frames[victim->second].second] = 0;
+        freeStack.push(frames[victim->second].second);
+        frames.erase(victim->second);
+        lastUsed.erase(victim);
     }
-    long index = freeList.front();
-    freeList.pop_front();
+    long index = freeStack.top();
+    freeStack.pop();
     frames[pageNum] = std::pair<long, int>(clock, index);
     lastUsed.insert(std::pair<long, long>(clock++, pageNum));
     physic_memory[index] = pageNum;
-    if (isFirst) {
-        isFirst = false;
-        pageReplace(physic_memory, nwAdd + (1 << PAGE_SHIFT));
-    }
-    else isFirst = true;
 }
 
