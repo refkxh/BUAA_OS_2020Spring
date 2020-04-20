ed1127e42483f7227b0b4522dd7281a13b196c5f
18373109 <refkxh@buaa.edu.cn>
2020-04-20 16:29:43 +0000

    No STL

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,10 +2,9 @@
 
 #include "pageReplace.h"
 
-#include <map>
-
 const unsigned int PAGE_SHIFT = 12;
 const unsigned int N_PHY_PAGE = 64;
+const unsigned int N_PAGE = 32 << 10;
 const int INF = 1e9 + 7;
 
 #define GET_PAGE(addr) (((unsigned long) (addr)) >> PAGE_SHIFT)
@@ -13,9 +12,9 @@ const int INF = 1e9 + 7;
 int top = 0;
 int freeStack[N_PHY_PAGE];  // physical index
 
-long lastUsed[N_PHY_PAGE]; // <physical index, last referenced time>
+long lastUsed[N_PHY_PAGE];  // <physical index, last referenced time>
 
-std::map<long, int> frames;  // <page number, physical index>
+int frames[N_PAGE];  // <page number, physical index>
 
 void pageReplace(long *physic_memory, long nwAdd) {
     static long clock = 0;
@@ -25,7 +24,7 @@ void pageReplace(long *physic_memory, long nwAdd) {
         }
     }
     long pageNum = GET_PAGE(nwAdd);
-    if (frames.count(pageNum)) {
+    if (frames[pageNum] > 0) {
         lastUsed[frames[pageNum]] = clock++;
         return;
     }
@@ -38,7 +37,7 @@ void pageReplace(long *physic_memory, long nwAdd) {
                 victim = i;
             }
         }
-        frames.erase(physic_memory[victim]);
+        frames[physic_memory[victim]] = 0;
         physic_memory[victim] = 0;
         freeStack[top++] = victim;
     }
