095ef9d731da4453e351dc71978a147b2a5c0f7a
18373109 <refkxh@buaa.edu.cn>
2020-05-18 07:54:39 +0000

    WHY???

--- init/init.c
+++ init/init.c
@@ -24,13 +24,13 @@ void mips_init()
 	// initialize exception vector.
 	trap_init();
 	// initialize PIT.
-	kclock_init();
+	//kclock_init();
 	kcons_init();
 
 	// Create process using macro 'ENC_CREATE'
 	// For more details about 'ENV_CREATE', see include/env.h
-	ENV_CREATE(user_fstest);
-	ENV_CREATE(fs_serv);
+	//ENV_CREATE(user_fstest);
+	//ENV_CREATE(fs_serv);
 
 	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
