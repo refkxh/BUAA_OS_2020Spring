13a022159148796d06f8ee4a608b518e1cc8f468
18373109 <refkxh@buaa.edu.cn>
2020-06-06 10:56:59 +0000

    Modified test

--- user/producer_consumer.c
+++ user/producer_consumer.c
@@ -1,43 +1,35 @@
 #include "lib.h"
 
-u_int produced = 0;
-u_int consumed = 0;
-
 void umain() {
+	u_int produced = 0;
+	u_int consumed = 0;
+	
 	u_int mutex = syscall_init_PV_var(1);
 	u_int empty = syscall_init_PV_var(10);
 	u_int full = syscall_init_PV_var(0);
+
 	u_int pid = fork();
 	while (1) {
 		if (pid) {
 			syscall_P(empty);
 			syscall_P(mutex);
 			writef("producing %dth item.\n", ++produced);
-			writef("now empty is %d and full is %d\n", syscall_check_PV_value(empty), syscall_check_PV_value(full));
+			writef("now empty is %d and full is %d\n.", syscall_check_PV_value(empty), syscall_check_PV_value(full));
 			syscall_V(mutex);
 			syscall_V(full);
-			if (produced >= 100) {
-				if (consumed >= 100) {
-					syscall_release_PV_var(mutex);
-					syscall_release_PV_var(produced);
-					syscall_release_PV_var(consumed);
-				}
-				return;
-			}
+			if (produced >= 100) return;
 		}
 		else {
 			syscall_P(full);
 			syscall_P(mutex);
 			writef("consuming %dth item.\n", ++consumed);
-			writef("now empty is %d and full is %d\n", syscall_check_PV_value(empty), syscall_check_PV_value(full));
+			writef("now empty is %d and full is %d\n.", syscall_check_PV_value(empty), syscall_check_PV_value(full));
 			syscall_V(mutex);
 			syscall_V(empty);
 			if (consumed >= 100) {
-				if (produced >= 100) {
-					syscall_release_PV_var(mutex);
-					syscall_release_PV_var(produced);
-					syscall_release_PV_var(consumed);
-				}
+				syscall_release_PV_var(mutex);
+				syscall_release_PV_var(empty);
+				syscall_release_PV_var(full);
 				return;
 			}
 		}
