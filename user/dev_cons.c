0f52891c930ee3b26a4336c4beec8bbf47838b69
18373109 <refkxh@buaa.edu.cn>
2020-06-24 02:50:38 +0000

    retry

--- user/dev_cons.c
+++ user/dev_cons.c
@@ -28,13 +28,12 @@ static void u_myoutput(void *arg, const char *s, int l)
 		syscall_write_dev(&s[i], 0x10000000, 1);
 
 		if (s[i] == '\n') {
-			syscall_write_dev('\n', 0x10000000, 1);
+			syscall_write_dev(&'\n', 0x10000000, 1);
 		}
 	}
 }
 
-void uwritef(char *fmt, ...){
-	return;
+void uwritef(char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
 	user_lp_Print(u_myoutput, 0, fmt, ap);
